c7bf3517c751ff23538aa8ea69f8de26621432b7
Zoraneko <tuandt1409@gmail.com>
2025-10-17 13:35:56 +0700

    chore: add NotFound page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Homepage } from "@/pages/Homepage";
-
+import { NotFound } from "@/pages/NotFound/NotFound";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
   // TODO: Add other routes here
   // Example:
